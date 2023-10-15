bdb0c8c350c79e6ad93886846228cbcdb1a0ac60
sahan Perera <sahan88@gmail.com>
2023-10-15 19:41:26 +0530

    Data seeding

---
+++ products/management/commands/add_data.py
@@ -0,0 +1,19 @@
+from typing import Any, Optional
+from django.core.management.base import BaseCommand
+from products.models import Product
+from customers.models import Customer
+
+class Command(BaseCommand):
+    def handle(self, *args: Any, **options: Any) -> str | None:
+
+        Product.objects.bulk_create([
+            Product(name="Engine", price=100),
+            Product(name="Door", price=40),
+        ])
+
+        Customer.objects.bulk_create([
+            Customer(name="Sahan", address="Colombo"),
+            Customer(name="Kamal", address="Kandy")
+        ])
+
+        print("Seeding successful.")
